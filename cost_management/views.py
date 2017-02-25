6a003a28f694eb00f00a85fbdfd020b3661782af
Harun Ur Rashid <harun1393@gmail.com>
2017-02-25 15:37:25 +0600

    backup

--- cost_management/views.py
+++ cost_management/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Expense
+from .forms import ExpenseForm
 
 
 def my_expense(request):
     expenses = Expense.objects.all()
-    print(expenses)
     context = {'expenses': expenses}
     return render(request, 'cost/expense.html', context)
+
+
+def add_expense(request):
+    form = ExpenseForm
+    return render(request, 'cost/add_expense.html', {'form': form})
